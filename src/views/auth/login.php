2bc7973789544ab02b17d27592cd8d9106a7da7a
Ian <ian.escarro@gmail.com>
2025-08-06 10:44:13 +0800

    add title

--- src/views/auth/login.php
+++ src/views/auth/login.php
@@ -8,7 +8,9 @@
 
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
 
-  <title></title>
+  <title>
+    <?php echo config_item('app_name'); ?> - Login
+  </title>
 
   <style>
     .login {
